d540443110ef36e5730f6c28248ad742a3ff313d
Errol Errolson <errol@toxic-ragers.co.uk>
2014-05-07 23:56:19 +0100

    Graphics.Entity: To be used for powerups, pedspawns and things of that nature

---
+++ Flummery/Graphics/Entity.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using OpenTK;
+
+namespace Flummery
+{
+    public enum AssetType
+    {
+        Sprite,
+        Model
+    }
+
+    public enum EntityType
+    {
+        Accessory,
+        Powerup
+    }
+
+    public class Entity
+    {
+        string uniqueIdentifier;
+        string name;
+        string tag;
+        EntityType entityType = EntityType.Powerup;
+        Matrix4 transform;
+        Asset asset;
+        AssetType assetType = AssetType.Model;
+
+        public string UniqueIdentifier
+        {
+            get { return uniqueIdentifier; }
+            set { uniqueIdentifier = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Tag
+        {
+            get { return tag; }
+            set { tag = value; }
+        }
+
+        public EntityType EntityType
+        {
+            get { return entityType; }
+            set { entityType = value; }
+        }
+
+        public Matrix4 Transform
+        {
+            get { return transform; }
+            set { transform = value; }
+        }
+    }
+}
