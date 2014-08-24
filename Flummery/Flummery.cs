625bf0bf362a66c0e0948a4ae1612302c16e439d
Errol Errolson <errol@toxic-ragers.co.uk>
2014-08-24 20:56:54 +0100

    v0.0.3.3

--- Flummery/Flummery.cs
+++ Flummery/Flummery.cs
@@ -8,6 +8,6 @@ namespace Flummery
         public static frmMain UI;
         public static bool Active;
         public static CultureInfo Culture = new CultureInfo("en-gb");
-        public static string Version = "0.0.3.2";
+        public static string Version = "0.0.3.3";
     }
 }
