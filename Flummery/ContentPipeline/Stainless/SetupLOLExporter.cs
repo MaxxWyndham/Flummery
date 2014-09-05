a7497acfd6606a203eea223958ba31a276b46d3a
Errol Errolson <errol@toxic-ragers.co.uk>
2014-09-06 00:37:40 +0100

    ContentPipeline.Stainless.SetupLOLExporter:  Created exporter for default vehicle setup.lol files

---
+++ Flummery/ContentPipeline/Stainless/SetupLOLExporter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+using ToxicRagers.CarmageddonReincarnation.Formats;
+
+namespace Flummery.ContentPipeline.Stainless
+{
+    class SetupLOLExporter : ContentExporter
+    {
+        public override void Export(Asset asset, string path)
+        {
+            var model = (asset as Model);
+
+            if (model.SupportingDocuments.ContainsKey("Setup"))
+            {
+                ((Setup)model.SupportingDocuments["Setup"]).Save(path);
+            }
+            else
+            {
+                var setup = new Setup();
+
+                switch (ExportSettings.GetSetting<SetupContext>("Context"))
+                {
+                    case SetupContext.Vehicle:
+                        setup.Settings = new VehicleSetupCode();
+                        break;
+                }
+
+                setup.Save(path);
+            }
+        }
+    }
+}
