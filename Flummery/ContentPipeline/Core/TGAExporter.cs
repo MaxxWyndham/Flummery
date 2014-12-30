767756d215b0586317c2ad2047fa1d2fa2b6fdd8
Errol Errolson <errol@toxic-ragers.co.uk>
2014-12-30 08:01:15 +0000

    TDX Convertor
    Imports: TDX, BMP, JPG, TIF, TGA, PNG
    Exports: TDX, BMP, JPG, TIF, PNG
    
    CommitLog:
    
    Finished the first pass of the TDX Convert dialog.  It is now possible to import TDX, BMP, JPG, TIF, TGA and PNG and export as TDX, BMP, JPG, TIF or PNG (TGA export coming soon)
    
    Closes #58

---
+++ Flummery/ContentPipeline/Core/TGAExporter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Flummery.ContentPipeline.Core
+{
+    class TGAExporter : ContentExporter
+    {
+        public override void Export(Asset asset, string Path)
+        {
+            // To Do
+        }
+    }
+}
