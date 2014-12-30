025c3817e6b9c0f65945e8531dfb3f1b8f557707
Errol Errolson <errol@toxic-ragers.co.uk>
2014-12-30 08:01:15 +0000

    Finished the first pass of the TDX Convert dialog.  It is now possible to import TDX, BMP, JPG, TIF, TGA and PNG and export as TDX, BMP, JPG, TIF or PNG (TGA export coming soon)

--- Flummery/frmTDXConvert.Designer.cs
+++ Flummery/frmTDXConvert.Designer.cs
@@ -28,20 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pbPreview = new System.Windows.Forms.PictureBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.lblFileSize = new System.Windows.Forms.Label();
+            this.cmdClose = new System.Windows.Forms.Button();
+            this.ofdBrowse = new System.Windows.Forms.OpenFileDialog();
+            this.sfdSave = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPreview)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pbPreview
+            // 
+            this.pbPreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbPreview.Location = new System.Drawing.Point(12, 12);
+            this.pbPreview.Name = "pbPreview";
+            this.pbPreview.Size = new System.Drawing.Size(512, 512);
+            this.pbPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbPreview.TabIndex = 2;
+            this.pbPreview.TabStop = false;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(530, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(186, 23);
+            this.btnOpen.TabIndex = 3;
+            this.btnOpen.Text = "open image...";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(530, 80);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(186, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "save as...";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Location = new System.Drawing.Point(530, 38);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(0, 13);
+            this.lblWidth.TabIndex = 5;
+            this.lblWidth.Tag = "Width: {0}";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(530, 51);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(0, 13);
+            this.lblHeight.TabIndex = 6;
+            this.lblHeight.Tag = "Height: {0}";
+            // 
+            // lblFileSize
+            // 
+            this.lblFileSize.AutoSize = true;
+            this.lblFileSize.Location = new System.Drawing.Point(530, 64);
+            this.lblFileSize.Name = "lblFileSize";
+            this.lblFileSize.Size = new System.Drawing.Size(0, 13);
+            this.lblFileSize.TabIndex = 7;
+            this.lblFileSize.Tag = "Filesize: {0}";
+            // 
+            // cmdClose
+            // 
+            this.cmdClose.Location = new System.Drawing.Point(530, 501);
+            this.cmdClose.Name = "cmdClose";
+            this.cmdClose.Size = new System.Drawing.Size(186, 23);
+            this.cmdClose.TabIndex = 8;
+            this.cmdClose.Text = "close";
+            this.cmdClose.UseVisualStyleBackColor = true;
+            this.cmdClose.Click += new System.EventHandler(this.cmdClose_Click);
+            // 
+            // ofdBrowse
+            // 
+            this.ofdBrowse.FileName = "openFileDialog1";
             // 
             // frmTDXConvert
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(860, 436);
+            this.ClientSize = new System.Drawing.Size(724, 536);
+            this.Controls.Add(this.cmdClose);
+            this.Controls.Add(this.lblFileSize);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.lblWidth);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.pbPreview);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "frmTDXConvert";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "frmTDXConvert";
+            this.Text = "TDX Convertor";
+            ((System.ComponentModel.ISupportInitialize)(this.pbPreview)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbPreview;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label lblWidth;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Label lblFileSize;
+        private System.Windows.Forms.Button cmdClose;
+        private System.Windows.Forms.OpenFileDialog ofdBrowse;
+        private System.Windows.Forms.SaveFileDialog sfdSave;
     }
 }
--- Flummery/frmTDXConvert.cs
+++ Flummery/frmTDXConvert.cs
@@ -1,19 +1,110 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Windows.Forms;
+
+using Flummery.ContentPipeline;
+using Flummery.ContentPipeline.Core;
+using Flummery.ContentPipeline.Stainless;
 
 namespace Flummery
 {
     public partial class frmTDXConvert : Form
     {
+        Texture t = null;
+
         public frmTDXConvert()
         {
             InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            ofdBrowse.Filter = "All supported files|*.jpg;*.png;*.tif;*.tga;*.bmp;*.tdx|JPG (*.jpg)|*.jpg|PNG (*.png)|*.png|TIF (*.tif)|*.tif|TGA (*.tga)|*.tga|BMP (*.bmp)|*.bmp|TDX (*.tdx)|*.tdx";
+
+            if (ofdBrowse.ShowDialog() == DialogResult.OK && File.Exists(ofdBrowse.FileName))
+            {
+                var fi = new FileInfo(ofdBrowse.FileName);
+
+                switch (fi.Extension)
+                {
+                    case ".bmp":
+                        t = SceneManager.Current.Content.Load<Texture, BMPImporter>(fi.Name, fi.DirectoryName);
+                        break;
+
+                    case ".jpg":
+                        t = SceneManager.Current.Content.Load<Texture, JPGImporter>(fi.Name, fi.DirectoryName);
+                        break;
+
+                    case ".png":
+                        t = SceneManager.Current.Content.Load<Texture, PNGImporter>(fi.Name, fi.DirectoryName);
+                        break;
+
+                    case ".tif":
+                        t = SceneManager.Current.Content.Load<Texture, TIFImporter>(fi.Name, fi.DirectoryName);
+                        break;
+
+                    case ".tga":
+                        t = SceneManager.Current.Content.Load<Texture, TGAImporter>(fi.Name, fi.DirectoryName);
+                        break;
+
+                    case ".tdx":
+                        t = SceneManager.Current.Content.Load<Texture, TDXImporter>(fi.Name, fi.DirectoryName);
+                        break;
+                }
+
+                if (t != null)
+                {
+                    var b = t.GetBitmap();
+
+                    pbPreview.Image = t.GetThumbnail();
+                    lblWidth.Text = string.Format(lblWidth.Tag.ToString(), b.Width);
+                    lblHeight.Text = string.Format(lblHeight.Tag.ToString(), b.Height);
+                    lblFileSize.Text = string.Format(lblFileSize.Tag.ToString(), fi.Length);
+                }
+            }
+        }
+
+        private void cmdClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            ContentExporter cx = null;
+            sfdSave.Filter = "TDX (*.tdx)|*.tdx|JPG (*.jpg)|*.jpg|PNG (*.png)|*.png|TIF (*.tif)|*.tif|BMP (*.bmp)|*.bmp";
+
+            if (sfdSave.ShowDialog() == DialogResult.OK)
+            {
+                switch (Path.GetExtension(sfdSave.FileName))
+                {
+                    case ".bmp":
+                        cx = new BMPExporter();
+                        break;
+
+                    case ".jpg":
+                        cx = new JPGExporter();
+                        break;
+
+                    case ".png":
+                        cx = new PNGExporter();
+                        break;
+
+                    case ".tif":
+                        cx = new TIFExporter();
+                        break;
+
+                    case ".tdx":
+                        cx = new TDXExporter();
+                        break;
+                }
+
+                if (cx != null)
+                {
+                    cx.Export(t, sfdSave.FileName);
+                    SceneManager.Current.UpdateProgress(string.Format("Saved {0}", sfdSave.FileName));
+                }
+            }
         }
     }
 }
