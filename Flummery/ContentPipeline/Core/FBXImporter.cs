64f9befc6075d61c481edc9696ca72395b971800
Maxx Wyndham <maxx@marketingradar.com>
2015-07-24 08:04:07 +0100

    ContentPipeline FBX Import: Flips imported meshes into the correct handedness

--- Flummery/ContentPipeline/Core/FBXImporter.cs
+++ Flummery/ContentPipeline/Core/FBXImporter.cs
@@ -590,6 +590,7 @@ namespace Flummery.ContentPipeline.Core
                 model.Santise();
 
                 if (worldMatrix != Matrix4.Identity) { ModelManipulator.Freeze(model, worldMatrix); }
+                ModelManipulator.FlipAxis(model.Root.Mesh, Axis.X, true);
 
                 return model;
             }
