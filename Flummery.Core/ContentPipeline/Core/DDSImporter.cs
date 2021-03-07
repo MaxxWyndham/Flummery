7ed59e4e1804422410a97b7dd6f5a0db52c53ee8
Maxx Wyndham <maxx@maxxwyndham.com>
2021-03-07 19:26:08 +0000

    Flummery.Core:  Added an importer for DDS files

---
+++ Flummery.Core/ContentPipeline/Core/DDSImporter.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+using ToxicRagers.Core.Formats;
+
+namespace Flummery.Core.ContentPipeline
+{
+    public class DDSImporter : ContentImporter
+    {
+        public override string GetExtension() { return "dds"; }
+
+        public override Asset Import(string path)
+        {
+            Texture texture = new Texture
+            {
+                FileName = path
+            };
+
+            texture.CreateFromBitmap(DDS.Load(path).Decompress(), Path.GetFileNameWithoutExtension(path));
+
+            return texture;
+        }
+    }
+}
