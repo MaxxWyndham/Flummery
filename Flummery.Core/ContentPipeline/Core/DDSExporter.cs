a71cd6b55e1103783f0731d4d5ddfbf379a6d17b
Maxx Wyndham <maxx@maxxwyndham.com>
2021-03-08 14:43:00 +0000

    Flummery.Core:  Added DDS exporter

---
+++ Flummery.Core/ContentPipeline/Core/DDSExporter.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+using System.IO;
+
+using ToxicRagers.Core.Formats;
+
+namespace Flummery.Core.ContentPipeline
+{
+    public class DDSExporter : ContentExporter
+    {
+        public override void Export(Asset asset, string path)
+        {
+            Texture texture = asset as Texture;
+            Bitmap b = texture.GetBitmap(false);
+
+            SceneManager.Current.UpdateProgress($"Saving {texture.Name}");
+
+            new DDS(ToxicRagers.Helpers.D3DFormat.DXT5, b).Save(path);
+
+            SceneManager.Current.UpdateProgress($"{Path.GetFileName(path)} saved!");
+        }
+    }
+}
